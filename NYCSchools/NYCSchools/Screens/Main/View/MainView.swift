d499367bb19a16437b25c340bdafe9b45d939e06
Giorgio Doganiero <doganierogiorgio@gmail.com>
2022-05-04 10:11:09 -0400

    Added banner to school list screen

--- NYCSchools/NYCSchools/Screens/Main/View/MainView.swift
+++ NYCSchools/NYCSchools/Screens/Main/View/MainView.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class MainView: UIView {
     // MARK: - Views
+    private var bannerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private var bannerTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "school_title_nyc_schools".localized
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .plain)
         tableView.rowHeight = UITableView.automaticDimension
@@ -38,10 +52,21 @@ class MainView: UIView {
         tableView.register(SchoolTableViewCell.self)
         
         // View layouts
+        addSubview(bannerView)
         addSubview(tableView)
+        bannerView.addSubview(bannerTitleLabel)
         
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: topAnchor),
+            bannerView.topAnchor.constraint(equalTo: topAnchor),
+            bannerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            bannerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            bannerTitleLabel.topAnchor.constraint(equalTo: bannerView.safeAreaLayoutGuide.topAnchor, constant: 16),
+            bannerTitleLabel.leadingAnchor.constraint(equalTo: bannerView.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            bannerTitleLabel.trailingAnchor.constraint(equalTo: bannerView.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            bannerTitleLabel.bottomAnchor.constraint(equalTo: bannerView.bottomAnchor, constant: -16),
+            
+            tableView.topAnchor.constraint(equalTo: bannerView.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
@@ -50,6 +75,8 @@ class MainView: UIView {
     
     private func setupThemeColor() {
         backgroundColor = .systemBackground
+        bannerView.backgroundColor = .systemPurple
+        bannerTitleLabel.textColor = .white
         tableView.backgroundColor = .clear
     }
 }
